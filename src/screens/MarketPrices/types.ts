fa3195ad0b2fde0373513c9064937fb9570ba247
Patrik Liška <patrik.liska@chyron.com>
2023-09-29 12:14:30 +0200

    feat: selectedItems separated into component

--- src/screens/MarketPrices/types.ts
+++ src/screens/MarketPrices/types.ts
@@ -7,3 +7,29 @@ export enum Quality {
 }
 
 export type QualityName = keyof typeof Quality;
+
+export interface Item {
+  name: string;
+  label: string;
+  enchantments: string[];
+}
+
+export type City = 'Bridgewatch' | 'Thetford' | 'Lymhurst' | 'Carleon' | 'Fort Sterling';
+
+export interface ItemPriceInformation {
+  buy_price_max: number;
+  buy_price_max_date: string;
+  buy_price_min: number;
+  buy_price_min_date: string;
+  city: City;
+  item_id: string;
+  quality: number;
+  sell_price_max: number;
+  sell_price_max_date: string;
+  sell_price_min: number;
+  sell_price_min_date: string;
+}
+
+export interface SelectedItem extends Item {
+  prices: ItemPriceInformation[];
+}
